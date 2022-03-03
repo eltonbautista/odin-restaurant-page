e03ac5331c1d0e941f10ccc98b8a4df86e684551
Elton Bautista <eltonbestaha@gmail.com>
2022-03-03 08:54:40 -0700

    rewrote my logic for creating list items, used an IIFE to return a method that creates a ul with 3 li food items, pretty proud of this one

--- src/menu.js
+++ src/menu.js
@@ -16,14 +16,32 @@ export function menu() {
     };
     [menuSides, menuMain, menuBev, menuFiller, menuMenu, menuResoName] = menuElements;
     // Each of our food items will be a list of 3 foods. We want a list that we can reuse. 
-    const foodList = document.createElement('ul');
-    const foodItems = []; // using const here because we want to preserve the value of foodItems being an array of <li>.
-    for (let i = 0; i < 3; i++) {
-        foodItems.push(document.createElement('li'));
-        foodList.appendChild(foodItems[i]);
+    const foodList = {
+        unorderedList: document.createElement('ul'),
+
     }
+    
     // now we have an array of li. Changing the values of those indices will affect the contents of the array
     // but ultimately our foodList should be a list of food items.  
 
-    
+    const foodList = (function() {
+
+        function menuCreator(...food) {
+        const foodList = document.createElement('ul');
+        const foodItems = [];
+        for (let i = 0; i < 3; i++) {
+            foodItems.push(document.createElement('li'));
+            foodList.appendChild(foodItems[i]);
+            foodItems[i].textContent = food[i];
+        }
+        return foodList;
+    }
+
+        return {
+            menuCreator,
+        }
+    })();
+
+    let sideFoods = foodList.menuCreator('$8 Yummy Eggs', '$12 Restaurant Wings', '$10 FeelsGood Potatoes'); 
+
 };
